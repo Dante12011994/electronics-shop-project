0639a9e5f3b0cc348eaa006668f9ff59d3d8293c
Dante <dante1201@mail.ru>
2023-05-02 19:08:07 +0300

    Написан класс Item Написаны тесты для проверки работы класса Item

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,11 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+def test_item():
+    test1 = Item('test', 1, 2)
+    assert test1.name == 'test'
+    Item.pay_rate = 2
+    test1.apply_discount()
+    assert test1.price == 2
+    assert test1.calculate_total_price() == 4
